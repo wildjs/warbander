b8a63b3d4793feb83fdb6d19a69514c65c5ec677
wildjs <johannes-wild@web.de>
2026-04-16 11:57:43 +0200

    More JSON Data

--- javascript/base.js
+++ javascript/base.js
@@ -3,10 +3,50 @@ let warband = {
     name: 'My Warband',
     units: [
         {
-            name: 'Captain'
+            name: 'Captain',
+            type: 'hero',
+            quantity: 1,
+            stats: {
+                adv: 5,
+                mar: 10,
+                dis: 6,
+                rat: 5,
+                upk: 3,
+                lvl: 8,
+                hp: 6,
+                def: 4,
+                res: 3,
+                arm: 0,
+                att: 1,
+                off: 4,
+                str: 3,
+                ap: 0,
+                agi: 6,
+                aim: 4,
+            },
         },
         {
-            name: 'Chicka'
+            name: 'Chicka',
+            type: 'henchman',
+            quantity: 7,
+            stats: {
+                adv: 5,
+                mar: 10,
+                dis: 10,
+                rat: 10,
+                upk: 10,
+                lvl: 10,
+                hp: 6,
+                def: 4,
+                res: 3,
+                arm: 0,
+                att: 1,
+                off: 4,
+                str: 3,
+                ap: 0,
+                agi: 6,
+                aim: 14,
+            },
         },
     ],
 };
@@ -17,7 +57,14 @@ function createTag(
     classNames
 ) {
     const element = document.createElement(tag);
-    element.class = classNames;
+    if (classNames) {
+        const splits = classNames.split(' ');
+        for (const split of splits) {
+            if (split) {
+                element.classList.add(split);
+            }
+        }
+    }
     return element;
 }
 
--- javascript/warband.js
+++ javascript/warband.js
@@ -3,12 +3,63 @@ async function toWarband() {
     h1.innerHTML = warband.name;
     main.appendChild(h1);
 
-    const container = createTag('section');
+    const container = createTag('section', 'warband');
     main.appendChild(container);
 
     for (const unit of warband.units) {
-        const unitContainer = createTag('div');
-        unitContainer.innerHTML = unit.name;
-        container.appendChild(unitContainer);
+        renderUnit(container, unit);
     }
+}
+
+function renderUnit(parent, unit) {
+    const color = unit.type === 'hero' ? 'purple' : 'blue';
+    const quantity = unit.quantity <= 1 ? '' : unit.quantity;
+    const html = `
+<div class="unit-header ${color}">${unit.name} <span>${quantity}<span></div>
+<div class="stats">
+    <div class="bright">Adv</div>
+    <div class="bright">Mar</div>
+    <div class="bright">Dis</div>
+    <div class="bright">Rat</div>
+    <div class="bright">Upk</div>
+    <div class="bright">Lvl</div>
+
+    <div>${unit.stats.adv}</div>
+    <div>${unit.stats.mar}</div>
+    <div>${unit.stats.dis}</div>
+    <div>${unit.stats.rat}</div>
+    <div>${unit.stats.upk}</div>
+    <div>${unit.stats.lvl}</div>
+
+    <div class="bright">HP</div>
+    <div class="bright">Def</div>
+    <div class="bright">Rest</div>
+    <div class="bright">Arm</div>
+    <div class="bright"></div>
+    <div class="bright"></div>
+    <div>${unit.stats.hp}</div>
+    <div>${unit.stats.def}</div>
+    <div>${unit.stats.res}</div>
+    <div>${unit.stats.arm}</div>
+    <div></div>
+    <div></div>
+
+    <div class="bright">Att</div>
+    <div class="bright">Off</div>
+    <div class="bright">Str</div>
+    <div class="bright">AP</div>
+    <div class="bright">Agi</div>
+    <div class="bright">Aim</div>
+
+    <div>${unit.stats.att}</div>
+    <div>${unit.stats.off}</div>
+    <div>${unit.stats.str}</div>
+    <div>${unit.stats.ap}</div>
+    <div>${unit.stats.agi}</div>
+    <div>${unit.stats.aim}</div>
+</div>
+    `;
+    const unitContainer = createTag('div', 'unit');
+    unitContainer.innerHTML = html;
+    parent.appendChild(unitContainer);
 }
