3805644e1e7c649cde4ed5142d48078558a035a8
wildjs <johannes-wild@web.de>
2026-04-17 08:32:46 +0200

    Created new index.html

--- merge.sh
+++ merge.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo 'Starting Build Process'
 
-cat ./build/header.html > ./build/index.html
-cat ./css/*.css >> ./build/index.html
-cat ./build/css.html >> ./build/index.html
-cat ./javascript/*.js >> ./build/index.html
-cat ./build/footer.html >> ./build/index.html
+cat ./build/header.html > index.html
+cat ./css/*.css >> index.html
+cat ./build/css.html >> index.html
+cat ./javascript/*.js >> index.html
+cat ./build/footer.html >> index.html
 
 echo 'Build done'
